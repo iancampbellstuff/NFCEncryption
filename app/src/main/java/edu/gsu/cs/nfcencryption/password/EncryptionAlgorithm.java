5607907adb969bc26c6a9fdf458d791a4f05052a
icampbell2 <icampbell2@student.gsu.edu>
2015-04-07 21:52:43 -0400

    The PBKDF2WithHmacSHA1 byte-size has been tuned to 128, and JavaDocs for this have been improved.
    Note that because randomly generated UUID's are being used for passwords, the length and byte-size can be sacrificed for faster performance as the UUID's will make the encryption much harder to crack.

--- app/src/main/java/edu/gsu/cs/nfcencryption/password/EncryptionAlgorithm.java
+++ app/src/main/java/edu/gsu/cs/nfcencryption/password/EncryptionAlgorithm.java
@@ -10,9 +10,16 @@ enum EncryptionAlgorithm {
 
     /**
      * See the <a href="https://en.wikipedia.org/wiki/PBKDF2">Wikipedia page on <em>PBKDF2</em></a>
-     * for reference.
+     * and the
+     * <a href="https://docs.oracle.com/javase/7/docs/technotes/guides/security/StandardNames.html#SecretKeyFactory">
+     * Oracle documentation on <code>SecretKeyFactory</code> algorithms</a> for reference.
+     *
+     * <em>Note that none of the other algorithms mentioned in the above-linked Oracle documentation
+     * are working, which seems to not be consistent across different JDK versions and operating
+     * systems -- see <a href="http://bugs.java.com/bugdatabase/view_bug.do?bug_id=7022467">this
+     * Oracle bug report</a></em> for reference.
      */
-    PBKDF2WithHmacSHA1("PBKDF2WithHmacSHA1", 100, 256);
+    PBKDF2WithHmacSHA1("PBKDF2WithHmacSHA1", 100, 128);
 
     /**
      *
